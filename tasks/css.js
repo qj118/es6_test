e88e7c2253341c0a62cf495769d4b817f4fd66b9
jin.qu <135885@moon-tech.com>
2020-05-22 21:31:17 +0800

    first commit

---
+++ tasks/css.js
@@ -0,0 +1,7 @@
+import gulp from 'gulp';
+
+
+gulp.task('css', () => {
+    return gulp.src('app/**/*.css')
+        .pipe(gulp.dest('server/public'))
+});
---
+++ tasks/scripts.js
@@ -0,0 +1,41 @@
+import gulp from 'gulp';
+import gulpif from 'gulp-if';
+import concat from 'gulp-concat';
+import webpack from 'webpack';
+import gulpWebpack from 'webpack-stream';
+import named from 'vinyl-named';
+import livereload from 'gulp-livereload';
+import plumber from 'gulp-plumber';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
+import {log, colors} from 'gulp-util';
+import args from './util/args';
+
+gulp.task('scripts', () => {
+    return gulp.src(['app/js/index.js']) // 读取文件
+        .pipe(plumber({
+            errorHandle: function(){
+            }
+        }))
+        .pipe(named())
+        .pipe(gulpWebpack({
+            module: {
+                loaders: [{
+                    test: /\.js$/,
+                    loader: 'babel'
+                }]
+            }
+        }), null, (err, stats) => {
+            log(`Finished '${colors.cyan('scripts')}'`, stats.toString({
+                chunks: false
+            }))
+        })
+        .pipe(gulp.dest('server/public/js')) // 保存文件
+        .pipe(rename({
+            basename: 'cp',
+            extname: '.min.js'
+        }))
+        .pipe(uglify({compress: {properties: false}, output: {'quote_keys': true}})) // 压缩文件
+        .pipe(gulp.dest('server/public/js')) // 压缩后再保存
+        .pipe(gulpif(args.watch, livereload())) // 监听刷新
+});
